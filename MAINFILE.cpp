0d0a1a104d148668e83eebc34f582b5ce533c050
Rachit Agrawal <rachitagrawal465@gmail.com>
2025-04-08 00:35:46 +0530

    wrote some classes and initial login not completed yet

--- MAINFILE.cpp
+++ MAINFILE.cpp
@@ -5,6 +5,7 @@ class person{
     protected:
     string name;
     int age ;
+    char gender ;
     string address;
     string contact;
 
@@ -35,6 +36,35 @@ class employee : public person{
 };
 
 int main(){
-    
+    int user;
+    char pat;
+    while(1){
+        cout<<"1. Enter as a Patient : "<<endl;
+        cout<<"2. Enter as a Employee : "<<endl;
+        cout<<"3. Return.... : "<<endl;
+        cout<<"YOUR RESPONSE : ";
+        cin>>user ;
+         
+        switch(user){
+            case 1:
+            cout<<"Hello Patient!!\n";
+            cout<<"Are you already registered previously (y/n) : ";
+            cin>>pat;
+            if(pat=='y'){
+                cout<<"Enter your patient id : ";
+                
+            }
+            else if(pat=='n'){
+                cout<<"Making a new registration : \n";
+                
+            }
+            else cout<<"Incorrect input kindly enter again "<<endl;
+
+            case 2:
+
+            case 3:
+
+        }
+    }
     return 0;
 }
