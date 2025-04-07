389f657b5598ad36245ae8fe26f50a10ee985c2e
Rachit Agrawal <rachitagrawal465@gmail.com>
2025-04-07 12:51:26 +0530

    added classes

--- MAINFILE.cpp
+++ MAINFILE.cpp
@@ -1,7 +1,40 @@
 #include<iostream>
 using namespace std;
-int main(){
-    cout<<"Hello this is our main file for HMS"<<endl;
-    cout<<"This line is written in testing branch"<<endl;
+
+class person{
+    protected:
+    string name;
+    int age ;
+    string address;
+    string contact;
+
+    public:
     
+};
+
+class patient : public person{
+    protected : 
+    string bloodgp;
+    string patientid;
+    int requiredep;
+
+    public :
+
+
+};
+
+class employee : public person{
+    protected : 
+    string empid;
+    int department;
+    
+
+    public :
+
+
+};
+
+int main(){
+    
+    return 0;
 }
