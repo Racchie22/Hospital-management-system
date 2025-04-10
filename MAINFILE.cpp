40e150119cd8d76b98315b0664c12e3a7d8ee685
Rachit Agrawal <rachitagrawal465@gmail.com>
2025-04-10 19:14:47 +0530

    writing patient interface

--- MAINFILE.cpp
+++ MAINFILE.cpp
@@ -21,19 +21,40 @@ class patient : public person{
 
     public :
 
+    void createpat(){
+        cout<<"Hello patient kindly enter the asked details correctly ! \n";
+        cout<<"Enter your Aame : ";
+        cin>>name;
+        cout<<"Enter your Age : ";
+        cin>>age;
+        cout<<"Enter your Gender : ";
+        cin>>gender;
+        cout<<"Enter your blood group : ";
+        cin>>bloodgp;
+        cout<<"Enter your contact details (10 digit valid phone number) : ";
+        cin>>contact;
+        cout<<"Enter your address : ";
+        cin >>address;
+        
+    }
 
+    void loadpat(string patid){
+
+    }
 };
 
 class employee : public person{
     protected : 
     string empid;
     int department;
-    
 
     public :
 
 
 };
+
+int findpatient(string patid);
+void patientchoices(patient* pat);
 
 int main(){
     int user;
@@ -48,15 +69,27 @@ int main(){
         switch(user){
             case 1:
             cout<<"Hello Patient!!\n";
-            cout<<"Are you already registered previously (y/n) : ";
+            cout<<"Have you registered previously (y/n) : ";
             cin>>pat;
             if(pat=='y'){
                 cout<<"Enter your patient id : ";
-                
+                string patid;
+                cin>>patid;
+                if(findpatient(patid)){
+                    patient* pat = new patient();
+                    pat->loadpat(patid);
+                }
+                else {
+                    cout<<"No Registered patients found "<<endl;
+                    cout<<"Kindly enter as a fresh patient ! \n"<<endl;
+                }
             }
             else if(pat=='n'){
                 cout<<"Making a new registration : \n";
-                
+                patient* pat = new patient();
+                pat->createpat();
+                cout<<"Registration done successfully !! \n"<<endl;
+                patientchoices(pat);
             }
             else cout<<"Incorrect input kindly enter again "<<endl;
 
